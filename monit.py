bda69e2da452490680c186a1c8452ebc1ea0439d
Camilo Polymeris <cpolymeris@gmail.com>
2015-01-10 10:44:54 -0300

    Connect to HTTPD

--- monit.py
+++ monit.py
@@ -3,12 +3,24 @@ import requests
 class Monit:
     def __init__(self, host='localhost', port=2812, username=None, password='', https=False):
         self.services = {}
+        self.baseurl = (https and 'https://%s:%i' or 'http://%s:%i') % (host, port)
+        self.auth = None
+        if username:
+            self.auth = requests.auth.HTTPBasicAuth(username, password)
+        self.update()
     
     def update(self):
-        pass
+        url = self.baseurl + '/_status?format=xml'
+        response = requests.get(url, auth=self.auth)
+        from xml.etree.ElementTree import XML
+        root = XML(response.text)
+        for serv_el in root.iter('service'):
+            serv = Monit.Service(self, serv_el)
+            self.services[serv.name] = serv
             
     class Service:
         def __init__(self, parent, xml):
+            self.name = xml.find('name').text
             self.running = None
             self.monitored = None
         
@@ -18,5 +30,5 @@ class Monit:
         def stop(self):
             pass
         
-        def monitor(self, monitor)
-        pass
+        def monitor(self, monitor):
+            pass
